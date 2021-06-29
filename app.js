44b2e7c2640c90d49f8cb3fc3dee4c728f621da3
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-06-29 16:57:11 +0300

    GET /

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 //DB connection
 const pool = require('./helper/db');
 
+app.use(express.json());
+
+//routes
+app.get('/', async (req, res) => {
+  pool
+    .query('SELECT * FROM learningpaths')
+    .then((lp) => {
+      res.json(lp.rows);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 app.listen(3000, () => {
   console.log('server is listening on port 3000');
 });
